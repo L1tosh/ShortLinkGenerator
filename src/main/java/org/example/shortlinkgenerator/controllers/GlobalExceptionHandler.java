3aa550f52ff25c43033849f25655d2524cafa7b2
L1tosh <dan.litosh33@gmail.com>
2024-06-20 15:04:48 +0300

    optimised exception handler

--- src/main/java/org/example/shortlinkgenerator/controllers/GlobalExceptionHandler.java
+++ src/main/java/org/example/shortlinkgenerator/controllers/GlobalExceptionHandler.java
@@ -8,20 +8,15 @@ import org.example.shortlinkgenerator.exceptions.UserNotFoundException;
 import org.example.shortlinkgenerator.utill.ResponseError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(ShortUrlCreateException.class)
+    @ExceptionHandler({ShortUrlCreateException.class, ConstraintViolationException.class, RegistrationException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    private ResponseEntity<ResponseError> handleException(ShortUrlCreateException e) {
+    private ResponseEntity<ResponseError> handleBadRequestException(RuntimeException e) {
         return new ResponseEntity<>(
                 createResponseError(e.getMessage()),
                 HttpStatus.BAD_REQUEST);
@@ -29,35 +24,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler({ShortUrlNotFoundException.class, UserNotFoundException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<ResponseError> handleException(RuntimeException e) {
+    public ResponseEntity<ResponseError> handleNotFoundException(RuntimeException e) {
         return new ResponseEntity<>(
                 createResponseError(e.getMessage()),
                 HttpStatus.NOT_FOUND);
     }
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<ResponseError> handleException(ConstraintViolationException e) {
-        return new ResponseEntity<>(
-                createResponseError(e.getMessage()),
-                HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(RegistrationException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<ResponseError> handleValidationExceptions(RegistrationException e) {
-        return new ResponseEntity<>(
-                createResponseError(e.getMessage()),
-                HttpStatus.BAD_REQUEST);
-    }
-
-//    @ExceptionHandler(UserNotFoundException.class)
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    public ResponseEntity<ResponseError> handleException(UserNotFoundException e) {
-//        return new ResponseEntity<>(
-//                createResponseError(e.getMessage()),
-//                HttpStatus.NOT_FOUND);
-//    }
 
     private ResponseError createResponseError(String errorMessage) {
         return new ResponseError(
